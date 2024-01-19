8fe18df1439a5d121984a4f04fb5c2c1b601ae90
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-19 16:42:05 -0600

    add first bread test to bread class

--- PierresBakery.Tests/BreadTests.cs
+++ PierresBakery.Tests/BreadTests.cs
@@ -6,5 +6,11 @@ namespace PierresBakery.Tests
     [TestClass]
     public class BreadTests
     {
+        [TestMethod]
+        public void BreadCost_CalculateSingleLoafCost_Int()
+        {
+            Assert.AreEqual(5, Bread.BreadCost(1));
+        }
+    }
 
-    }}
+}
