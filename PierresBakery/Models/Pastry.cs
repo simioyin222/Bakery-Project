02fbc5bebc0bfcf9d53db22974a6d538c0baa48a
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-25 03:31:52 -0600

    remove commented out code

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -17,8 +17,5 @@ namespace PierresBakery.Models
         }
     }
 }
-
-//  auto implementing proprties in the classes, example how many loaves/ or quanity 1 per class  like get set like price or discount info , want a seprate method to caculate total cost using those methods, should be caculating method for both bread and pastry 
-// add read.me 
-// caculating total but it needs to be associated with construct like bread. then caculate total rather just all in one thing 
+ 
 
