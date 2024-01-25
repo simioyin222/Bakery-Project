e4a4a0ba2761d133259ae78b6a115b2489464055
simioyin222 <simi.oyinkolade@gmail.com>
2024-01-25 00:43:37 -0600

    new pastry test red factor

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -13,5 +13,18 @@ namespace PierresBakery.Tests
             Assert.AreEqual(typeof(Pastry), pastryOrder.GetType());
         }
 
+        [TestMethod]
+        public void CalculateTotalCost_ReturnsCorrectTotalCostForSinglePastry_Int()
+        {
+            Pastry pastryOrder = new Pastry(1);
+            Assert.AreEqual(2, pastryOrder.CalculateTotalCost());
+        }
+
+        [TestMethod]
+        public void CalculateTotalCost_ReturnsCorrectTotalCostForFourPastries_Int()
+        {
+            Pastry pastryOrder = new Pastry(4);
+            Assert.AreEqual(6, pastryOrder.CalculateTotalCost());
+        }
     }
 }
